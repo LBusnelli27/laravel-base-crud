da93308ec426e7b4fcb1a413c152b7338505143e
Busna27 <lucabusnelli2001@gmail.com>
2022-05-17 14:35:32 +0200

    make model

---
+++ app/Http/Controllers/ComicController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\comic;
+use Illuminate\Http\Request;
+
+class ComicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\comic  $comic
+     * @return \Illuminate\Http\Response
+     */
+    public function show(comic $comic)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\comic  $comic
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(comic $comic)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\comic  $comic
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, comic $comic)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\comic  $comic
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(comic $comic)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_05_17_123149_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->text("description")->nullable();
+            $table->string("thumb");
+            $table->float("price", 6);
+            $table->date("sale_date");
+            $table->string("type");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
